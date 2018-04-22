109775690f026344d5377e13dc26567abeb392f7
Ashish Tomer <ashish.tomer@knoldus.in>
2018-04-22 17:29:03 +0530

    Made the build.sbt more clean

---
+++ project/Dependencies.scala
@@ -0,0 +1,11 @@
+import sbt._
+
+object Dependencies {
+  val akkaStream = "com.typesafe.akka" %% "akka-stream" % "2.5.12"
+  val akkaActor = "com.typesafe.akka" %% "akka-actor" % "2.5.12"
+  val typesafeConfig = "com.typesafe" % "config" % "1.3.2"
+  val akkaStreamContrib = "com.typesafe.akka" %% "akka-stream-contrib" % "0.9"
+  val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5" % Test
+  val akkaTestKit = "com.typesafe.akka" %% "akka-testkit" % "2.5.12" % Test
+  val akkaStreamTestKit = "com.typesafe.akka" %% "akka-stream-testkit" % "2.5.12" % Test
+}
